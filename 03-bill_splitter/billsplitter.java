2d4c60cc059c6c8eae113de54c2cdc5f45c0a569
zhifeng29 <72803271+zhifeng29@users.noreply.github.com>
2020-10-25 14:56:12 -0400

    Update billsplitter.java

--- 03-bill_splitter/billsplitter.java
+++ 03-bill_splitter/billsplitter.java
@@ -4,21 +4,21 @@ public class Buggy{
 
 	public static void main(String[] args) {
 
-		Scanner input = new Scanner(System.in);
-		System.out.println("Welcome to the Bill-Splitter");
-		System.out.print("Enter the total, pre-tax: ");
+    Scanner input = new Scanner(System.in);
+    System.out.println("Welcome to the Bill-Splitter");
+    System.out.print("Enter the total, pre-tax: ");
     double total = input.nextDouble();
     System.out.println("enter the text rate (a number that is 1-100)");
     double tax = input.nextDouble();
     tax = tax /100 + 1;
     System.out.println("enter the amount of tip you pay");
     double tips  = input.nextDouble();
-		System.out.print("How many people? ");
+    System.out.print("How many people? ");
     int numpeople = input.nextInt();
 
     total = total * tax + tips;
 
-		double perPerson = total/(double)numpeople;
+    double perPerson = total/(double)numpeople;
 
 
     System.out.printf(" Each person must pay $ %.2f", perPerson);
